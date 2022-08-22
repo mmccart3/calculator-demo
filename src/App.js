d925775d2053f9dbef5132db139c7b5b9b7e0db4
philomeno <106666729+philomeno@users.noreply.github.com>
2022-08-22 14:55:11 +0100

    latest copy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react"
+import calcButtons from "./buttons.json"
+import {evaluate} from "mathjs"
+// import Setsum2 from "./modules/Setsumfunc"
 
-function App() {
+
+
+const App = () => {
+  const [mySum,setMySum] = useState([0])
+  let tmpSum = [0]
+  let tmp2Sum = 0
+  let tmp3Sum = 0
+
+  
+  function handleButton (val) {
+    if(val==="clear") {setMySum(0)}
+    else if (val === "="){
+      tmpSum = mySum.join("")
+      tmp2Sum = evaluate(tmpSum)
+      tmp3Sum = Math.floor(tmp2Sum*10000000)/10000000
+     // limits number of decimal places displayed
+      setMySum([tmp3Sum])}
+    else {
+        tmpSum = [...mySum, val]
+        // spread operator needed to take a copy of the data not just a reference
+        if ((tmpSum[0] === 0) && (tmpSum[1] !==".")) {tmpSum.shift()}
+        // decimal point check added to all decimal numbers less than one
+        setMySum(tmpSum)
+        }
+        
+    }
+  
+
+  
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Calculator</h1>
+
+      <div className="calcWrapper">
+        <div className="screen">{mySum}</div>
+        <div className="buttons">
+        {calcButtons.map((item, index) => (
+          <button className={`btn ${item.style}`} key={index} onClick={() => handleButton(item.val)}> {item.alias ? item.alias: item.val}
+          </button>
+        ))}
+      </div>
     </div>
-  );
-}
+  </div>
+  )
+}        
 
 export default App;
